504a46295f73d1c3ee313b3c9e89be50372ac88e
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-12-23 13:05:46 -0300

    exercicio 1 feito com testes

---
+++ Exercicio1.java
@@ -0,0 +1,17 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        //TODO: Declare as variáveis abaixo
+        int idade = 16;
+        double altura = 1.87;
+        String nome = "Enzo";
+        boolean ativo = true;
+        char inicial = 'E';
+
+        //TODO: Exiba os valores
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Nome: " + nome);
+        System.out.println("Ativo: " + ativo);
+        System.out.println("Inicial: " + inicial);
+    }
+}
